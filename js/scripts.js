b08be0eed0a03c816ec72d1210a5d615a00b150e
ada2030 <64311043+ada2030@users.noreply.github.com>
2021-02-23 18:34:29 +0300

    Верстка страницы Коментарии и Настройки в Личном кабинете и Правки.

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,18 @@ $(document).ready(function() {
     });
 });
 
+var hamburger = document.querySelector(".js-hamburger");
+var menuMobile = document.querySelector(".js-main-menu");
+
+if(menuMobile) {
+    menuMobile.classList.add("hidden");
+    hamburger.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        hamburger.classList.toggle("open");
+        menuMobile.classList.toggle("open");
+    });
+}
+
 var frontend = new function() {
     var self = this;
 
